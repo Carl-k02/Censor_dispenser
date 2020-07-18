2165670346b43127c8a15f98de1756a72dc9f4e2
Carl Kanani <Carlkanani@hotmail.co.uk>
2020-07-18 20:15:11 +0100

    censored multiple words in email 2

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -4,8 +4,22 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-
+# first part, censoring a word and replacing it wiih another word
 def censor_email(censored_word, replaced_with):
     censored_email = email_one.replace(censored_word, replaced_with)
     return censored_email
-print(censor_email('learning algorithms', 'banana'))
+#print(censor_email('learning algorithms', 'banana'))
+
+#Second part, sensoring multiple words from an email
+proprietary_terms = ["she", "personality matrix", "sense of self",
+                     "self-preservation", "learning algorithm",
+                     "her", "herself"]
+range1 = range(len(proprietary_terms) - 1)
+email2_lower = email_two.lower()
+
+censored_email2 = email2_lower.replace(proprietary_terms[0], "banana").replace(proprietary_terms[1], "banana").replace(proprietary_terms[2], "banana").replace(proprietary_terms[3], "banana").replace(proprietary_terms[4], "banana").replace(proprietary_terms[5], "banana").replace(proprietary_terms[6], "banana")
+
+    
+
+print(censored_email2)
+    
